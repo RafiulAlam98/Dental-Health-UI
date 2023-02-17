d2d14b20491af6b427e6a8e473475ca7927b5b23
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-02-17 23:31:20 +0600

    firebase data added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h2>this is login</h2>
+    <div className="h-[500px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-4xl text-center">Login</h2>
+
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">What is your name?</span>
+            </label>
+            <input
+              {...register("name")}
+              type="email"
+              className="input input-bordered w-full "
+            />
+          </div>
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              {...register("name")}
+              type="password"
+              className="input input-bordered w-full "
+            />
+            <label className="label">
+              <span className="label-text">Forget Password</span>
+            </label>
+          </div>
+
+          <input
+            type="submit"
+            value="Login"
+            className="btn w-full btn-accent text-white"
+          />
+        </form>
+        <p>
+          New to Doctor's Portal?{" "}
+          <Link to="/signup" className="text-secondary">
+            Create a new account
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">Continue With Google</button>
+      </div>
     </div>
   );
 };
